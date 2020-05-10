bf1cdaf1f35fde24e31a409f08fbd796f80790e1
Gareth Cary <gareth@caryfamily.co.uk>
2020-05-11 00:51:40 +0100

    style contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
+      :subject => "Alexandra Lansdale - Wedding Singer Email",
       :to => "gareth@caryfamily.co.uk",
       :from => %("#{name}" <#{email}>)
     }
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+  get 'create', to: 'contacts#create'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get "songs", to: "songs#index"
